5586f9f648b9b43f0e8d800b83ed2c88e97c50c3
Susan <kotoamatsukami33@gmail.com>
2025-06-15 09:18:59 +0545

    static

---
+++ js/slider.js
@@ -0,0 +1,37 @@
+$('.slider-container').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  
+  });
+
+
+
